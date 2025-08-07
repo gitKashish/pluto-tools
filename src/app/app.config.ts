26785ee4426da144d137d230a6a0879ad57bfd58
gitKashish <125540624+gitKashish@users.noreply.github.com>
2025-08-08 02:52:38 +0530

    ADD: PrimeNG with Noir theme and Inter Font

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,25 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { providePrimeNG } from 'primeng/config';
+import { Noir } from './app.theme';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      ripple: true,
+      theme: {
+          preset: Noir,
+          options: {
+            darkModeSelector: 'light'
+          }
+      }
+    }),
   ]
 };

27a2bf11a80bafbf6f64dc31ed82babb76a30e49
Kashish Sahu <125540624+gitKashish@users.noreply.github.com>
2025-08-17 04:51:55 +0530

    ADD: basic formatting functionality

--- src/app/features/json-formatter/json-formatter.ts
+++ src/app/features/json-formatter/json-formatter.ts
@@ -1,19 +1,24 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 // UI components
 import { Splitter } from 'primeng/splitter';
+import { Toast } from 'primeng/toast';
 
+
+import { MessageService } from 'primeng/api';
 import { ToolbarService, ToolbarState } from '@app/core/toolbar';
 
 @Component({
   selector: 'app-json-formatter',
-  imports: [Splitter, FormsModule],
+  imports: [Splitter, FormsModule, Toast],
   templateUrl: './json-formatter.html',
-  styleUrl: './json-formatter.scss'
+  styleUrl: './json-formatter.scss',
 })
 export class JsonFormatter implements OnInit, OnDestroy {
+  // dependencies
   toolbar = inject(ToolbarService);
+  messageService = inject(MessageService);
 
   // actions
   toolbarState: ToolbarState = {
@@ -23,11 +28,35 @@ export class JsonFormatter implements OnInit, OnDestroy {
         id: 'json-formatter-format',
         title: 'Format Raw JSON',
         keywords: ['format', 'run', 'pretty'],
-        handler() {
-          console.log('Formatting JSON');
-        },
+        handler: () => {
+          this.formatRawJson();
+        }
       }
     ]
+  }
+
+  // states
+  rawJsonString = signal('');
+  jsonObject = signal<Object>({});
+  formattedJson: string = '';
+
+  formatRawJson() {
+    try {
+      this.messageService.clear('json-format-error');
+      this.jsonObject = JSON.parse(this.rawJsonString());
+      this.formattedJson = JSON.stringify(this.jsonObject, null, '  ');
+    } catch (error: any) {
+      console.error(error.message);
+      this.messageService.add(
+        {
+          severity: 'error',
+          summary: 'JSON Format Error',
+          detail: error.message,
+          sticky: true,
+          key: 'json-format-error'
+        }
+      )
+    }
   }
 
   ngOnInit(): void {
